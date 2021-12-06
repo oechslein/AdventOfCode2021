8a6eef17b4aac6072a927cfb24a7d4437908d0b2
Christoph Oechslein <oechslein@garmin.com>
2021-12-06 14:34:20 +0100

    Add a much quicker solution
    Change-Id: I2160457ae4d2064b13b992bedfe9fefa475dc032

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -14,16 +14,19 @@ use std::error;
 use std::fmt::Debug;
 use std::fs;
 use std::io;
+use std::mem;
 use std::ops::Sub;
 use std::path::Path;
 use std::str::{FromStr, Split};
 use std::time::{Duration, Instant};
 
-use num::Zero;
-use test::Bencher;
 use counter::Counter;
 use itertools::Itertools;
-use num::bigint::{BigUint,ToBigUint};
+use test::Bencher;
+
+use num::bigint::{BigUint, ToBigUint};
+use num::Zero;
+
 
 #[macro_use]
 extern crate lazy_static;
@@ -34,29 +37,25 @@ mod utils;
 /// The main function prints out the results for part1 and part2 of the day01
 /// AOC
 fn main() -> Result<(), Box<dyn error::Error>> {
-    utils::with_measure("Part 1", || {
-        solve_part1(utils::file_to_string("input.txt"))
-    });
-    utils::with_measure("Part 2", || {
-        solve_part2(utils::file_to_string("input.txt"))
-    });
+    utils::with_measure("Part 1", || solve_part1(utils::file_to_string("input.txt")));
+    utils::with_measure("Part 2", || solve_part2(utils::file_to_string("input.txt")));
     Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 type NumberType = BigUint;
-type FishPopulationType = Counter<NumberType, NumberType>;
 
+fn solve_it_slow(content: String, max_days: u32) -> NumberType {
+    type FishPopulationType = Counter<NumberType, NumberType>;
 
-fn solve_it(content: String, max_days: u32) -> NumberType {
     lazy_static! {
         static ref AFTER_BREAD_LIFE_TIME: NumberType = 6.to_biguint().unwrap();
         static ref NEW_FISH_LIFE_TIME: NumberType = 8.to_biguint().unwrap();
     }
     let mut pop: FishPopulationType = content
-    .split(",")
-    .map(|n| n.trim().parse::<NumberType>().unwrap())
-    .collect();
+        .split(",")
+        .map(|n| n.trim().parse::<NumberType>().unwrap())
+        .collect();
     //println!("pop: {:?}", pop);
 
     for _day in 1..=max_days {
@@ -65,11 +64,11 @@ fn solve_it(content: String, max_days: u32) -> NumberType {
             Some(breading_fish_amount) => {
                 next_day_pop[&AFTER_BREAD_LIFE_TIME] += &breading_fish_amount;
                 next_day_pop[&NEW_FISH_LIFE_TIME] += &breading_fish_amount;
-            },
+            }
             None => {}
         };
         for (days_until_bread, frequency) in pop.into_iter() {
-            let days_until_bread_minus_one = days_until_bread-1.to_biguint().unwrap();
+            let days_until_bread_minus_one = days_until_bread - 1.to_biguint().unwrap();
             next_day_pop[&days_until_bread_minus_one] += frequency;
         }
         //println!("day: {:?}, next_day_pop: {:?}", _day, next_day_pop);
@@ -77,7 +76,45 @@ fn solve_it(content: String, max_days: u32) -> NumberType {
         pop = next_day_pop;
     }
 
-   pop.iter().map(|(_, frequency)| frequency).sum::<NumberType>() as NumberType
+    pop.iter()
+        .map(|(_, frequency)| frequency)
+        .sum::<NumberType>() as NumberType
+}
+
+fn solve_it(content: String, max_days: u32) -> NumberType {
+    /*
+    Part 1 result: 0 (elapsed time is: 614.8┬╡s)
+Part 2 result: 0 (elapsed time is: 24.3508195s)
+*/
+    let mut pop = [
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+        BigUint::zero(),
+    ];
+
+    content
+        .split(",")
+        .map(|n| n.trim().parse::<usize>().unwrap())
+        .for_each(|days_until_bread| {
+            pop[days_until_bread] += 1.to_biguint().unwrap();
+        });
+
+    for _day in 1..=max_days {
+        for i in 1..=8 {
+            pop.swap(i - 1, i);
+        }
+        // pop[8] has now the value from pop[0]!
+        pop[6] += pop[8].clone();
+    }
+
+    //pop.iter().sum()
+    BigUint::zero()
 }
 
 /// The part1 function calculates the result for part1
@@ -88,6 +125,7 @@ fn solve_part1(content: String) -> Result<NumberType, String> {
 /// The part2 function calculates the result for part2
 fn solve_part2(content: String) -> Result<NumberType, String> {
     Ok(solve_it(content, 10_000_000))
+    //Ok(solve_it(content, 256))
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
