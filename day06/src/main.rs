ea5e4c0ddde7b555d9a28c25b69a9ba8e6c56fa9
Christoph Oechslein <oechslein@garmin.com>
2021-12-06 17:43:08 +0100

    Clean up
    Change-Id: Ic9179ca4d6b1a3e4e32e7ebe61476c44126c95d7

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -10,6 +10,7 @@ extern crate test;
 
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
+use std::convert::TryInto;
 use std::error;
 use std::fmt::Debug;
 use std::fs;
@@ -24,9 +25,7 @@ use counter::Counter;
 use itertools::Itertools;
 use test::Bencher;
 
-use num::bigint::{BigUint, ToBigUint};
-use num::Zero;
-
+use ibig::{ibig, modular::ModuloRing, ubig, UBig};
 
 #[macro_use]
 extern crate lazy_static;
@@ -43,78 +42,48 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-type NumberType = BigUint;
 
-fn solve_it_slow(content: String, max_days: u32) -> NumberType {
-    type FishPopulationType = Counter<NumberType, NumberType>;
+/*
+type NumberType = UBig;
 
-    lazy_static! {
-        static ref AFTER_BREAD_LIFE_TIME: NumberType = 6.to_biguint().unwrap();
-        static ref NEW_FISH_LIFE_TIME: NumberType = 8.to_biguint().unwrap();
-    }
-    let mut pop: FishPopulationType = content
-        .split(",")
-        .map(|n| n.trim().parse::<NumberType>().unwrap())
-        .collect();
-    //println!("pop: {:?}", pop);
+fn _create_number(x: u64) -> NumberType {
+    ubig!(x)
+}
+*/
 
-    for _day in 1..=max_days {
-        let mut next_day_pop = FishPopulationType::new();
-        match pop.remove(&NumberType::zero()) {
-            Some(breading_fish_amount) => {
-                next_day_pop[&AFTER_BREAD_LIFE_TIME] += &breading_fish_amount;
-                next_day_pop[&NEW_FISH_LIFE_TIME] += &breading_fish_amount;
-            }
-            None => {}
-        };
-        for (days_until_bread, frequency) in pop.into_iter() {
-            let days_until_bread_minus_one = days_until_bread - 1.to_biguint().unwrap();
-            next_day_pop[&days_until_bread_minus_one] += frequency;
-        }
-        //println!("day: {:?}, next_day_pop: {:?}", _day, next_day_pop);
+type NumberType = u64;
 
-        pop = next_day_pop;
-    }
-
-    pop.iter()
-        .map(|(_, frequency)| frequency)
-        .sum::<NumberType>() as NumberType
+fn _create_number(x: u64) -> NumberType {
+    x
 }
 
+
 fn solve_it(content: String, max_days: u32) -> NumberType {
-    /*
-    Part 1 result: 0 (elapsed time is: 614.8┬╡s)
-Part 2 result: 0 (elapsed time is: 24.3508195s)
-*/
-    let mut pop = [
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-        BigUint::zero(),
-    ];
+    const YOUNG_FISH_BREADING_CYCLE_TIME: usize = 8;
+    const FISH_BREADING_CYCLE_TIME: usize = 6;
+    //let mut pop: [NumberType; YOUNG_FISH_BREADING_CYCLE_TIME+1] = [_create_number(0); YOUNG_FISH_BREADING_CYCLE_TIME];
+    let mut pop: [NumberType; YOUNG_FISH_BREADING_CYCLE_TIME + 1] =
+        itertools::repeat_n(_create_number(0), YOUNG_FISH_BREADING_CYCLE_TIME + 1)
+            .collect::<Vec<NumberType>>()
+            .try_into()
+            .unwrap();
 
     content
         .split(",")
         .map(|n| n.trim().parse::<usize>().unwrap())
         .for_each(|days_until_bread| {
-            pop[days_until_bread] += 1.to_biguint().unwrap();
+            pop[days_until_bread] += _create_number(1);
         });
 
     for _day in 1..=max_days {
-        for i in 1..=8 {
+        for i in 1..=YOUNG_FISH_BREADING_CYCLE_TIME {
             pop.swap(i - 1, i);
         }
-        // pop[8] has now the value from pop[0]!
-        pop[6] += pop[8].clone();
+        // pop[YOUNG_FISH_BREADING_CYCLE_TIME] has now the value from pop[0]!
+        pop[FISH_BREADING_CYCLE_TIME] += pop[YOUNG_FISH_BREADING_CYCLE_TIME].clone();
     }
 
-    //pop.iter().sum()
-    BigUint::zero()
+    pop.iter().sum()
 }
 
 /// The part1 function calculates the result for part1
@@ -124,8 +93,8 @@ fn solve_part1(content: String) -> Result<NumberType, String> {
 
 /// The part2 function calculates the result for part2
 fn solve_part2(content: String) -> Result<NumberType, String> {
-    Ok(solve_it(content, 10_000_000))
-    //Ok(solve_it(content, 256))
+    //Ok(solve_it(content, 10_000_000))
+    Ok(solve_it(content, 256))
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -139,7 +108,7 @@ mod tests {
     fn test1() -> Result<(), Box<dyn error::Error>> {
         assert_eq!(
             solve_part1(utils::file_to_string("test.txt")).unwrap(),
-            5934
+            _create_number(5934)
         );
         Ok(())
     }
@@ -148,7 +117,7 @@ mod tests {
     fn test2() -> Result<(), Box<dyn error::Error>> {
         assert_eq!(
             solve_part2(utils::file_to_string("test.txt")).unwrap(),
-            26984457539
+            _create_number(26984457539)
         );
         Ok(())
     }
@@ -157,7 +126,7 @@ mod tests {
     fn verify1() -> Result<(), Box<dyn error::Error>> {
         assert_eq!(
             solve_part1(utils::file_to_string("input.txt")).unwrap(),
-            5632
+            _create_number(362666)
         );
         Ok(())
     }
@@ -166,7 +135,7 @@ mod tests {
     fn verify2() -> Result<(), Box<dyn error::Error>> {
         assert_eq!(
             solve_part2(utils::file_to_string("input.txt")).unwrap(),
-            22213
+            _create_number(1640526601595)
         );
         Ok(())
     }
