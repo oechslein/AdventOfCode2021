9dfd037a86be0bb212d110b4cda1d0ff3dc7b5d0
Christoph Oechslein <christoph.oechslein@garmin.com>
2021-12-20 20:45:41 +0100

    Day 20 - first solution

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -17,7 +17,6 @@ use std::error;
 use std::fmt::{Debug, Display};
 use std::fs::File;
 use std::ops::{Add, RangeFrom, SubAssign};
-use std::str::FromStr;
 
 mod utils;
 
@@ -38,53 +37,52 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 //#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 #[derive(Debug, Clone)]
 struct Image {
-    _data: HashMap<(isize, isize), bool>,
-    _transform_map: HashMap<usize, bool>,
-    _min_x_y: (isize, isize),
-    _max_x_y: (isize, isize),
-    _default: bool,
+    _data: HashSet<(isize, isize)>,
 }
 
 impl Image {
-    fn new(transform_map: HashMap<usize, bool>) -> Image {
+    fn new() -> Image {
         Image {
-            _data: HashMap::new(),
-            _transform_map: transform_map,
-            _min_x_y: (isize::MAX, isize::MAX),
-            _max_x_y: (isize::MIN, isize::MIN),
-            _default: false,
+            _data: HashSet::new(),
         }
     }
-
-    fn new_from_image(&self) -> Image {
-        Image {
-            _data: HashMap::new(),
-            _transform_map: self._transform_map.clone(),
-            _min_x_y: (isize::MAX, isize::MAX),
-            _max_x_y: (isize::MIN, isize::MIN),
-            _default: if !self._default {
-                self._transform_map[&0]
-            } else {
-                self._transform_map[&(1 << 8)]
-            },
-        }
-    }
-
-    fn get(&mut self, x: isize, y: isize) -> bool {
-        **self._data.get(&(x, y)).get_or_insert(&self._default)
+    fn get(&self, x: isize, y: isize) -> bool {
+        self._data.get(&(x, y)).is_some()
     }
 
     fn set(&mut self, x: isize, y: isize, new_value: bool) {
-        self._min_x_y = (self._min_x_y.0.min(x), self._min_x_y.1.min(y));
-        self._max_x_y = (self._max_x_y.0.max(x), self._max_x_y.1.max(y));
-        self._data.insert((x, y), new_value);
+        if new_value {
+            self._data.insert((x, y));
+        }
     }
 
     fn get_minmax(&self) -> ((isize, isize), (isize, isize)) {
-        (self._min_x_y, self._max_x_y)
+        let mut min_x_y = (isize::MAX, isize::MAX);
+        let mut max_x_y = (isize::MIN, isize::MIN);
+        for (x, y) in self._data.iter() {
+            min_x_y = (min_x_y.0.min(*x), min_x_y.1.min(*y));
+            max_x_y = (max_x_y.0.max(*x), max_x_y.1.max(*y));
+        }
+        (min_x_y, max_x_y)
     }
 
-    fn get_neighbors_as_number(&mut self, x: isize, y: isize) -> usize {
+    fn get_minmax_for_steps(&self, i: isize, steps: isize) -> ((isize, isize), (isize, isize)) {
+        let (min_x_y, max_x_y) = self.get_minmax();
+        let (min_x_y, max_x_y) = if i == 0 {
+            (
+                (min_x_y.0 - 2 * steps, min_x_y.1 - 2 * steps),
+                (max_x_y.0 + 2 * steps, max_x_y.1 + 2 * steps),
+            )
+        } else {
+            (
+                (min_x_y.0 - 1, min_x_y.1 - 1),
+                (max_x_y.0 + 1, max_x_y.1 + 1),
+            )
+        };
+        (min_x_y, max_x_y)
+    }
+
+    fn get_neighbors_as_number(&self, x: isize, y: isize) -> usize {
         let mut result = 0;
         for (new_y, new_x) in (y - 1..=y + 1).cartesian_product(x - 1..=x + 1) {
             result *= 2;
@@ -95,11 +93,11 @@ impl Image {
         result
     }
 
-    fn iter(&self) -> impl Iterator<Item = (&(isize, isize), &bool)> {
+    fn iter(&self) -> impl Iterator<Item = &(isize, isize)> {
         self._data.iter()
     }
 
-    fn display(&mut self) {
+    fn display(&self) {
         let (min_x_y, max_x_y) = self.get_minmax();
         for y in min_x_y.1..max_x_y.1 {
             for x in min_x_y.0..max_x_y.0 {
@@ -113,7 +111,7 @@ impl Image {
         }
     }
 
-    fn save_pgn(&mut self, file_name: &str, (min_x_y, max_x_y): ((isize, isize), (isize, isize))) {
+    fn save_pgn(&self, file_name: &str, (min_x_y, max_x_y): ((isize, isize), (isize, isize))) {
         let width = (max_x_y.0 - min_x_y.0) as u32;
         let height = (max_x_y.1 - min_x_y.1) as u32;
         let img = ImageBuffer::from_fn(width, height, |x, y| {
@@ -126,7 +124,7 @@ impl Image {
         img.save(file_name).unwrap();
     }
 
-    fn create_frame(&mut self, (min_x_y, max_x_y): ((isize, isize), (isize, isize))) -> Frame {
+    fn create_frame(&self, (min_x_y, max_x_y): ((isize, isize), (isize, isize))) -> Frame {
         let width = (max_x_y.0 - min_x_y.0) as u32;
         let height = (max_x_y.1 - min_x_y.1) as u32;
         let img = ImageBuffer::from_fn(width, height, |x, y| {
@@ -138,38 +136,30 @@ impl Image {
         });
         Frame::from_parts(img, width, height, Delay::from_numer_denom_ms(100, 1))
     }
-
-    fn count_trues(&self) -> usize {
-        self._data.values().filter(|value| **value).count()
-    }
 }
 
-impl FromStr for Image {
-    type Err = ();
+fn parse(content: &str) -> (HashMap<usize, bool>, Image) {
+    let content = content.replace('\r', "");
+    let (transform_str, image_str) = content.trim().split("\n\n").collect_tuple().unwrap();
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let s = s.replace('\r', "");
-        let (transform_str, image_str) = s.trim().split("\n\n").collect_tuple().unwrap();
+    let transform = transform_str
+        .chars()
+        .enumerate()
+        .map(|(index, c)| (index, c == '#'))
+        .collect();
+    let mut image = Image::new();
 
-        let transform: HashMap<usize, bool> = transform_str
-            .chars()
-            .enumerate()
-            .map(|(index, c)| (index, c == '#'))
-            .collect();
-        let mut image = Image::new(transform);
-
-        for (y, line) in image_str.split('\n').enumerate() {
-            for (x, c) in line.chars().enumerate() {
-                image.set(x as isize, y as isize, c == '#')
-            }
+    for (y, line) in image_str.split('\n').enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            image.set(x as isize, y as isize, c == '#')
         }
-
-        Ok(image)
     }
+
+    (transform, image)
 }
 
-fn solve(file_name: &str, steps: isize, gif_file_name: Option<&str>) -> usize {
-    let mut image = Image::from_str(&utils::file_to_string(file_name)).unwrap();
+fn solve(file_name: &str, steps: isize, gif_file_name: &str) -> usize {
+    let (transform_to_pixel, mut image) = parse(&utils::file_to_string(file_name));
 
     //println!("{} {} {}", empty_result, full_result, 1 << 8);
     let (start_min_x_y, start_max_x_y) = image.get_minmax();
@@ -177,62 +167,59 @@ fn solve(file_name: &str, steps: isize, gif_file_name: Option<&str>) -> usize {
         (start_min_x_y.0 - steps, start_min_x_y.1 - steps),
         (start_max_x_y.0 + steps, start_max_x_y.1 + steps),
     );
-    let mut frames = Vec::new();
-    for _i in 0..steps {
-        let mut new_image = image.new_from_image();
-        let (min_x_y, max_x_y) = image.get_minmax();
-        //println!("Step: {} {:?}", _i + 1, (min_x_y, max_x_y));
+    //let mut frames = Vec::new();
+    for i in 0..steps {
+        let mut new_image = Image::new();
+        let (min_x_y, max_x_y) = image.get_minmax_for_steps(i, steps);
+        //println!("Step: {} ({:?})", i + 1, (min_x_y, max_x_y));
         //image.display();
-        if let Some(_gif_file_name) = gif_file_name {
-            //image.save_pgn(format!("{}_{}.png", _gif_file_name, _i).as_str(), final_size);
-            frames.push(image.create_frame(final_size));
-        }
+        //image.save_pgn(format!("{}_{}.png", gif_file_name, i).as_str(), final_size);
+        //frames.push(image.create_frame(final_size));
 
-        for y in min_x_y.1 - 1..=max_x_y.1 + 1 {
-            for x in min_x_y.0 - 1..=max_x_y.0 + 1 {
-                let get_neighbors_as_number = image.get_neighbors_as_number(x, y);
-                new_image.set(x, y, image._transform_map[&get_neighbors_as_number]);
+        for y in min_x_y.1..=max_x_y.1 {
+            for x in min_x_y.0..=max_x_y.0 {
+                new_image.set(
+                    x,
+                    y,
+                    transform_to_pixel[&image.get_neighbors_as_number(x, y)],
+                );
             }
         }
         image = new_image;
     }
 
-    //let (min_x_y, max_x_y) = image.get_minmax();
-    //println!("Result: {:?}", (min_x_y, max_x_y));
+    let (min_x_y, max_x_y) = image.get_minmax();
+    //println!("Result: ({:?})", (min_x_y, max_x_y));
     //image.display();
-    if let Some(gif_file_name) = gif_file_name {
-        image.save_pgn(
-            format!("{}_FULL.png", gif_file_name).as_str(),
-            image.get_minmax(),
-        );
-        frames.push(image.create_frame(final_size));
-        save_animated_gif(gif_file_name, frames);
-    }
+    //image.save_pgn(
+    //    format!("{}_FULL.png", gif_file_name).as_str(),
+    //    image.get_minmax(),
+    //);
+    //frames.push(image.create_frame(final_size));
+    //let file_out = File::create(gif_file_name).unwrap();
+    //let mut encoder = GifEncoder::new(file_out);
+    //encoder.encode_frames(frames.into_iter());
 
-    image.count_trues()
-}
-
-fn save_animated_gif(gif_file_name: &str, frames: Vec<Frame>) {
-    let file_out = File::create(gif_file_name).unwrap();
-    let mut encoder = GifEncoder::new(file_out);
-    encoder.encode_frames(frames.into_iter());
+    let result = image
+        .iter()
+        .filter(|(x, y)| {
+            *x >= start_min_x_y.0 - steps
+                && *y >= start_min_x_y.1 - steps
+                && *x <= start_max_x_y.0 + steps
+                && *y <= start_max_x_y.1 + steps
+        })
+        .count();
+    result
 }
 
 fn solve_part1(file_name: &str) -> usize {
-    //solve(file_name, 2, Some(format!("part1_{}.gif", file_name).as_str()))
-    solve(file_name, 2, None)
+    let result = solve(file_name, 2, format!("part1_{}.gif", file_name).as_str());
+    result
 }
 
 fn solve_part2(file_name: &str) -> usize {
-    //let result = solve(file_name, 50, Some(format!("part2_{}.gif", file_name).as_str()));
-    let result = solve(file_name, 50, None);
-    /*
-    solve(
-        "test.txt",
-        200,
-        Some(format!("partX_{}.gif", "test.txt").as_str()),
-    );
-    */
+    let result = solve(file_name, 50, format!("part2_{}.gif", file_name).as_str());
+    //solve(file_name, 1000, format!("partX_{}.gif", file_name).as_str());
     result
 }
 
