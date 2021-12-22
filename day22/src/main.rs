014fcccfb80b365de643e7b0f1e9fe3d52832582
Christoph Oechslein <oechslein@garmin.com>
2021-12-22 16:49:13 +0100

    Cleanup
    Change-Id: Ice50690ba4ae95876aec77b0b3fd1bd006513078

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -10,7 +10,6 @@
 extern crate test;
 
 use itertools::Itertools;
-use num_complex::Complex64;
 
 use core::num;
 use std::collections::{HashMap, HashSet, VecDeque};
@@ -47,39 +46,14 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
 #[derive(Debug, Clone)]
 struct Cuboid {
-    x_range: RangeInclusive<isize>,
-    y_range: RangeInclusive<isize>,
-    z_range: RangeInclusive<isize>,
+    x_range: Interval<isize>,
+    y_range: Interval<isize>,
+    z_range: Interval<isize>,
     holes: Vec<Cuboid>,
 }
 
-fn parse_interval(s: &str) -> RangeInclusive<isize> {
-    let x: (isize, isize) = s[2..]
-        .split("..")
-        .map(|s| s.parse().unwrap())
-        .collect_tuple()
-        .unwrap();
-    x.0..=x.1
-}
-
-fn parse(file_name: &str) -> Vec<(bool, Cuboid)> {
-    utils::file_to_lines(file_name)
-        .filter(|s| !s.is_empty())
-        .map(|s| {
-            let (s1, s2) = s.split(' ').collect_tuple().unwrap();
-            let is_on = "on" == s1;
-            let (x, y, z) = s2.split(',').map(parse_interval).collect_tuple().unwrap();
-            (is_on, Cuboid::new(x, y, z))
-        })
-        .collect_vec()
-}
-
 impl Cuboid {
-    fn new(
-        x_range: RangeInclusive<isize>,
-        y_range: RangeInclusive<isize>,
-        z_range: RangeInclusive<isize>,
-    ) -> Cuboid {
+    fn new(x_range: Interval<isize>, y_range: Interval<isize>, z_range: Interval<isize>) -> Cuboid {
         Self {
             x_range,
             y_range,
@@ -88,11 +62,28 @@ impl Cuboid {
         }
     }
 
-    fn trim(&mut self, limit: usize) {
-        let limit = limit as isize;
-        self.x_range = (*self.x_range.start()).max(-limit)..=(*self.x_range.end()).min(limit);
-        self.y_range = (*self.y_range.start()).max(-limit)..=(*self.y_range.end()).min(limit);
-        self.z_range = (*self.z_range.start()).max(-limit)..=(*self.z_range.end()).min(limit);
+    fn _parse_interval(s: &str) -> Interval<isize> {
+        let (axis_lower, axis_upper): (isize, isize) = s[2..]
+            .split("..")
+            .map(|s| s.parse().unwrap())
+            .collect_tuple()
+            .unwrap();
+        Interval::new(axis_lower, axis_upper)
+    }
+
+    fn from(s: &str) -> Cuboid {
+        let (x, y, z) = s
+            .split(',')
+            .map(Cuboid::_parse_interval)
+            .collect_tuple()
+            .unwrap();
+        Cuboid::new(x, y, z)
+    }
+
+    fn intersect_axis(&mut self, limit_interval: Interval<isize>) {
+        self.x_range = self.x_range.intersection(&limit_interval);
+        self.y_range = self.y_range.intersection(&limit_interval);
+        self.z_range = self.z_range.intersection(&limit_interval);
     }
 
     fn contains(&self, x: &isize, y: &isize, z: &isize) -> bool {
@@ -102,67 +93,71 @@ impl Cuboid {
             && !self.holes.iter().any(|h| h.contains(x, y, z))
     }
 
+    fn size(&self) -> usize {
+        let cuboid_size = self.x_range.size() * self.y_range.size() * self.z_range.size();
+        let holes_size: usize = self.holes.iter().map(Cuboid::size).sum();
+        cuboid_size - holes_size
+    }
+
+    fn is_empty(&self) -> bool {
+        self.x_range.is_empty() || self.y_range.is_empty() || self.z_range.is_empty()
+    }
+
+    fn intersection(&self, other: &Self) -> Self {
+        Cuboid::new(
+            self.x_range.intersection(&other.x_range),
+            self.y_range.intersection(&other.y_range),
+            self.z_range.intersection(&other.z_range),
+        )
+    }
+
     fn remove_cuboid(&mut self, other: &Self) {
+        // remove cuboid from holes below (a hole is a negative /off cuboid, a hole of a negative cuboid is positive / on again)
         for hole in &mut self.holes {
             hole.remove_cuboid(other);
         }
-        if let Some(new_hole) = self.intersect(other) {
+
+        // add hole only if intersects
+        let new_hole = self.intersection(other);
+        if !new_hole.is_empty() {
             self.holes.push(new_hole);
         }
     }
+}
 
-    fn intersect_range(
-        range: &RangeInclusive<isize>,
-        other_range: &RangeInclusive<isize>,
-    ) -> RangeInclusive<isize> {
-        *range.start().max(other_range.start())..=*range.end().min(other_range.end())
-    }
-
-    fn intersect(&self, other: &Self) -> Option<Self> {
-        let x_range = Cuboid::intersect_range(&self.x_range, &other.x_range);
-        let y_range = Cuboid::intersect_range(&self.y_range, &other.y_range);
-        let z_range = Cuboid::intersect_range(&self.z_range, &other.z_range);
-
-        if x_range.is_empty() || y_range.is_empty() || z_range.is_empty() {
-            return None;
-        }
-
-        Some(Cuboid::new(x_range, y_range, z_range))
-    }
-
-    fn calc_ons(&self) -> usize {
-        // this could be removed if ExactSizeIterator is implemented for (u|i)size.
-        let calc_size = |range: &RangeInclusive<isize>| {
-            if range.is_empty() {
-                0
-            } else {
-                (range.end() - range.start() + 1) as usize
-            }
-        };
-        let hole_offs: usize = self.holes.iter().map(|c| c.calc_ons()).sum();
-        (calc_size(&self.x_range) * calc_size(&self.y_range) * calc_size(&self.z_range)) - hole_offs
-    }
+fn parse(file_name: &str) -> Vec<(bool, Cuboid)> {
+    utils::file_to_lines(file_name)
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            let (s1, s2) = s.split(' ').collect_tuple().unwrap();
+            let is_on = "on" == s1;
+            (is_on, Cuboid::from(s2))
+        })
+        .collect_vec()
 }
 
 fn apply_instructions(instructions: Vec<(bool, Cuboid)>) -> usize {
     let mut cuboids_on = Vec::<Cuboid>::new();
-    for (is_on, c) in instructions {
-        let new_cuboid = Cuboid::new(c.x_range, c.y_range, c.z_range);
-        for cuboid in cuboids_on.iter_mut() {
-            cuboid.remove_cuboid(&new_cuboid)
+
+    for (is_on, cuboid) in instructions {
+        // remove new cubiod from existing on Buboids
+        for cuboid_on in cuboids_on.iter_mut() {
+            cuboid_on.remove_cuboid(&cuboid);
         }
 
+        // add all on cubiods
         if is_on {
-            cuboids_on.push(new_cuboid);
+            cuboids_on.push(cuboid);
         }
     }
-    cuboids_on.iter().map(Cuboid::calc_ons).sum()
+    cuboids_on.iter().map(Cuboid::size).sum()
 }
-
 
 fn solve_part1(file_name: &str) -> usize {
     let mut instructions = parse(file_name);
-    instructions.iter_mut().for_each(|(_, c)| c.trim(50));
+    instructions
+        .iter_mut()
+        .for_each(|(_, c)| c.intersect_axis(Interval::new(-50, 50)));
     apply_instructions(instructions)
 }
 
@@ -170,7 +165,6 @@ fn solve_part2(file_name: &str) -> usize {
     let instructions = parse(file_name);
     apply_instructions(instructions)
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////
 #[cfg(test)]
